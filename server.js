b8bb92bbb5a777656843936b41a99404358606d5
Shreesha99 <shreeshavenkatram99@gmail.com>
2024-09-11 20:57:04 +0530

    feat: add initial project files and configurations

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cors = require('cors');  
+require('dotenv').config();
+
+const app = express();
+
+// CORS Options
+const corsOptions = {
+  origin: ['https://www.fuzemantra.com', 'http://localhost:3000'],
+  methods: ['GET', 'POST'],
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));  // Enable CORS for the specified origin
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Successfully connected to MongoDB!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Error connecting to MongoDB:', err.message);
+});
+
+// Define User Schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Create User Model
+const User = mongoose.model('FuzeUser', userSchema);
+
+// Register Endpoint
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists!' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully!' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Login Endpoint
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      res.status(200).json({ message: 'User logged in successfully!' });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Export the app to be used as a serverless function
+module.exports = app;
